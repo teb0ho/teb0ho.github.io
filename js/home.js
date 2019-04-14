61b73e0eff0704ba50eb235055a60cc8f0541654
Teboho  Mphure <Teboho@Tebohos-MacBook-Pro.local>
2019-04-14 11:10:14 +0200

    edit home intro text and make footer dynamic

--- js/home.js
+++ js/home.js
@@ -1,2 +1,5 @@
-var contactform = document.getElementById('contactform');
+let contactform = document.getElementById('contactform');
+let footerText = document.querySelector('span.text-muted');
+let currentYear = new Date().getFullYear();
 contactform.setAttribute('action', '//formspree.io/' + 'tebohomphure' + '@' + 'gmail' + '.' + 'com');
+footerText.innerHTML = `2018 - ${currentYear} Teboho Mphure`;
